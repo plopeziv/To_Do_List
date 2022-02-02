c83bec785040a6a7602b9eb24ddc0eae917919b6
plopeziv <plopez@8thlight.com>
2022-02-02 13:04:47 -0600

    Update local prop for ListItem

--- to-do-app/src/ListItem.js
+++ to-do-app/src/ListItem.js
@@ -3,16 +3,25 @@ import React, { Component } from 'react';
 class ListItem extends Component {
   constructor(props) {
     super(props);
+
+    this.handleClick = this.updateItemStatus.bind(this);
   }
-
+  
+  updateItemStatus() {
+    this.props.itemProperties.completed = !this.props.itemProperties.completed
+  }
+  
   render() {
-
     return (
         <div className="List-Item-Container">
             <img src="/minus-button.png" className="Action-Button"></img>
             <div className = "To-Do-Space">{this.props.itemProperties.toDoItem}</div>
             <div className = "List-Item-Button">
-                <input type="checkbox" className='List-Checkbox'></input>
+                <input 
+                    type="checkbox" className='List-Checkbox' 
+                    defaultChecked={this.props.itemProperties.completed}
+                    onChange={this.handleClick}>
+                </input>
             </div>
         </div>
     );
--- to-do-app/src/ListItem.test.js
+++ to-do-app/src/ListItem.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ListItem from './ListItem';
-import { render, screen } from "@testing-library/react"
+import { render, screen, fireEvent } from "@testing-library/react"
+import { shallow } from "enzyme";
 
 test("renders items from to-do list", () => {
   const filteredJson = {"toDoItem": "First Item", "completed": false}
@@ -28,4 +29,40 @@ test("checkbox is rendered once", () => {
   const checkbox = screen.getAllByRole("checkbox");
 
   expect(checkbox).toHaveLength(1);
+});
+
+test("checkbox is marked if item is done", () => {
+  const filteredJson = {"toDoItem": "First Item", "completed": true}
+
+  render(<ListItem itemProperties = {filteredJson}/>);
+
+  const checkbox = screen.getByRole("checkbox");
+
+  expect(checkbox.checked).toBeTruthy();
+});
+
+test("checkbox completed switches states after click", () => {
+  const filteredJson = {"toDoItem": "First Item", "completed": true}
+
+  render(<ListItem itemProperties = {filteredJson}/>);
+
+  const checkbox = screen.getByRole("checkbox");
+
+  fireEvent.click(checkbox);
+
+  expect(checkbox.checked).toBeFalsy();
+});
+
+test("changes the status of active list", () => {
+  const filteredJson = {"toDoItem": "First Item", "completed": false}
+
+  const wrapper = shallow(<ListItem itemProperties={filteredJson}/>);
+  
+  render(<ListItem itemProperties={filteredJson}/>);
+
+  const checkbox = screen.getByRole("checkbox");
+
+  fireEvent.click(checkbox);
+
+  expect(wrapper.instance().props.itemProperties).toStrictEqual({"toDoItem": "First Item", "completed": true});
 });
