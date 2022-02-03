aef3d0341f6953d6ab872b6b1d43dc5b03d8df6b
plopeziv <plopez@8thlight.com>
2022-02-03 13:13:37 -0600

    fix ToDoApp file path

--- to-do-app/src/index.js
+++ to-do-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import ToDoApp from './App';
+import ToDoApp from './ToDoApp';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
